3e4a375c1b6d8f0b0c8522522ca660ba66229c0b
Twang629 <225612322+Twang629@users.noreply.github.com>
2025-11-10 12:48:50 -0600

    Script start

---
+++ index.js
@@ -0,0 +1,10 @@
+const options = ["rock", "paper","scissors"];
+
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}   
+
+getComputerChoice();
+
+console.log(choice);
