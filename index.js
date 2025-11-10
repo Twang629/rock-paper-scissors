e526df66e79121e49ab5af636c1908073c7591dc
Twang629 <225612322+Twang629@users.noreply.github.com>
2025-11-10 15:41:32 -0600

    Completed. Mostly.

--- index.js
+++ index.js
@@ -5,6 +5,82 @@ function getComputerChoice(){
     return choice;
 }   
 
-getComputerChoice();
+function checkWinner(playerSelection,computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock") )
+    {
+        return "Player";
+    }
+    else{
+        return "Computer";
+    }
+}
 
-console.log(choice);
+function playRound(playerSelection,computerSelection){
+    const result = checkWinner(playerSelection,computerSelection);
+    if(result == "Tie"){
+        return "Tie. Drat!";
+    }
+    else if(result == "Player"){
+        return `${playerSelection} beats ${computerSelection}. You win this round.`;
+    }
+    else if(result=="Computer"){
+        return `${computerSelection} beats ${playerSelection}. Bummer.`;
+    }
+}
+
+function getPlayerChoice(){
+    let validatedInput = false;
+
+    while(validatedInput == false){
+        const choice = prompt("rock, paper, or scissors");
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(choice.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+
+    for(let i = 0; i < 5; i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection,computerSelection));
+        console.log("...");
+
+        if(checkWinner(playerSelection,computerSelection) == "Player"){
+            scorePlayer++;
+        }
+        else if (checkWinner(playerSelection,computerSelection) == "Computer"){
+            scoreComputer++
+        }
+
+        if(scorePlayer>scoreComputer){
+            console.log(`You are winning ${scorePlayer} - ${scoreComputer}. You got this!`);
+        }
+        else if (scoreComputer > scorePlayer){
+            console.log(`You are losing ${scorePlayer} - ${scoreComputer}. Don't lose hope!`);
+        }
+        else{
+            console.log(`All tied up at ${scorePlayer}`);
+        }
+    }
+}
+
+//const playerSelection = "rock";
+
+game();
+
