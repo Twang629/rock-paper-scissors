df947e2890fb7202058c244182dd2931906088b7
Twang629 <225612322+Twang629@users.noreply.github.com>
2025-11-10 22:04:17 -0600

    comment practice

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const options = ["rock", "paper","scissors"];
 
 function getComputerChoice(){
-    const choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
+    const choice = options[Math.floor(Math.random() * options.length)]; //Gets random number from 0 to .999..., then multiplies by 3 (array legth). Rounds down
+    return choice;                                                      //to 0, 1, or 2. This selects the respective array index.
 }   
 
-function checkWinner(playerSelection,computerSelection){
+function checkWinner(playerSelection,computerSelection){ //compares selections, determines and returns winner.
     if(playerSelection == computerSelection){
         return "Tie";
     }
-    else if(
+    else if( //all win conditions
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper") ||
         (playerSelection == "paper" && computerSelection == "rock") )
@@ -21,7 +21,7 @@ function checkWinner(playerSelection,computerSelection){
     }
 }
 
-function playRound(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){ //displays round result winner
     const result = checkWinner(playerSelection,computerSelection);
     if(result == "Tie"){
         return "Tie. Drat!";
@@ -29,7 +29,7 @@ function playRound(playerSelection,computerSelection){
     else if(result == "Player"){
         return `${playerSelection} beats ${computerSelection}. You win this round.`;
     }
-    else if(result=="Computer"){
+    else if(result == "Computer"){
         return `${computerSelection} beats ${playerSelection}. Bummer.`;
     }
 }
@@ -38,11 +38,11 @@ function getPlayerChoice(){
     let validatedInput = false;
 
     while(validatedInput == false){
-        const choice = prompt("rock, paper, or scissors");
+        const choice = prompt("rock, paper, or scissors"); //prompts user for choice
         if(choice == null){
             continue;
         }
-        const choiceInLower = choice.toLowerCase();
+        const choiceInLower = choice.toLowerCase(); //sets input to lowercase
         if(choice.includes(choiceInLower)){
             validatedInput = true;
             return choiceInLower;
@@ -61,14 +61,14 @@ function game(){
         console.log(playRound(playerSelection,computerSelection));
         console.log("...");
 
-        if(checkWinner(playerSelection,computerSelection) == "Player"){
+        if(checkWinner(playerSelection,computerSelection) == "Player"){ //increments scoreboard (ties not tracked)
             scorePlayer++;
         }
         else if (checkWinner(playerSelection,computerSelection) == "Computer"){
             scoreComputer++
         }
 
-        if(scorePlayer>scoreComputer){
+        if(scorePlayer>scoreComputer){ //console reports current score
             console.log(`You are winning ${scorePlayer} - ${scoreComputer}. You got this!`);
         }
         else if (scoreComputer > scorePlayer){
@@ -79,8 +79,6 @@ function game(){
         }
     }
 }
-
-//const playerSelection = "rock";
 
 game();
 
